9e9c8315cff28fe828bc96958574366e95dacd11
amlanxbghn <amlanxconnect@gmail.com>
2024-08-09 00:50:13 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { BiLogoMongodb } from 'react-icons/bi';
+import { FaGitAlt, FaHtml5, FaJava, FaLinux, FaNode, FaReact } from 'react-icons/fa';
+import { RiNextjsLine } from 'react-icons/ri';
+import { SiExpress, SiJavascript, SiMysql, SiSpringboot, SiTailwindcss } from 'react-icons/si';
 
 const About = () => {
   return (
@@ -9,15 +13,32 @@ const About = () => {
             Hi
           </div>
           <div className="w-[50%]">
-            <p className='font-medium text-black text-[26px] leading-[38px]'>Born in <a href="https://www.google.com/search?q=Assam" target="_blank" rel="noopener noreferrer">Assam</a>, and currently thriving in <a href="https://www.google.com/search?q=Bengaluru" target="_blank" rel="noopener noreferrer" className='underline'>Bengaluru</a>, I am a sophomore at <a href="https://www.scaler.com/school-of-technology/" target="_blank" rel="noopener noreferrer" className='underline'>Scaler School of Technology</a>, with 1+ years of experience as a <span className='underline'>developer</span>.</p>
-            <p className='font-medium text-black text-[26px] leading-[38px] mt-6'>I enjoy creating impactful digital experiences and incorporating <span className='underline'>design</span> into my work. My focus is on making digital experiences that resonate with users and create lasting impressions.</p>  
+            <p className='font-medium text-black text-[26px] leading-[38px]'>
+                Born in <a href="https://www.google.com/search?q=Assam" target="_blank" rel="noopener noreferrer">Assam</a>, and currently thriving in <a href="https://www.google.com/search?q=Bengaluru" target="_blank" rel="noopener noreferrer" className='underline'>Bengaluru</a>. I am a sophomore at <a href="https://www.scaler.com/school-of-technology/" target="_blank" rel="noopener noreferrer" className='underline'>Scaler School of Technology</a>, with 1+ years of experience as a <span className='underline'>developer</span>.
+            </p>
+            <p className='font-medium text-black text-[26px] leading-[38px] mt-6'>
+                I enjoy creating impactful digital experiences and incorporating <span className='underline'>design</span> into my work. My focus is on making digital experiences that resonate with users and create lasting impressions.
+            </p>  
           </div>
         </div>
       </div>
-      <div>
-        <p className="h-[20vh] text-center text-[20px] font-medium text-[#767676] leading-[28px]">
+      <div className="flex flex-col flex-grow items-center">
+        <p className="text-center text-[20px] font-medium text-[#767676] leading-[28px]">
             Technical Skills & Frameworks
         </p>
+        <div className='flex-grow flex items-center justify-between w-[70%]'>
+            <FaJava className='text-5xl'/>
+            <SiJavascript className='text-5xl'/>
+            <FaLinux className='text-5xl'/>
+            <BiLogoMongodb className='text-5xl'/>
+            <SiExpress className='text-5xl'/>
+            <FaReact className='text-5xl'/>
+            <FaNode className='text-5xl'/>
+            <SiTailwindcss className='text-5xl'/>
+            <RiNextjsLine className='text-5xl'/>
+            <FaGitAlt className='text-5xl'/>
+            <SiMysql className='text-5xl'/>
+        </div>
       </div>
     </div>
   );
