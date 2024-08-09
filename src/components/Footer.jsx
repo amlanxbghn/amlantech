d0682aedbeb174ff010f58d9f21cce62b0b65f88
amlanxbghn <amlanxconnect@gmail.com>
2024-08-09 21:58:29 +0530

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="mt-10 h-[10vh] text-[#767676] flex justify-center items-center">
+      <p className="text-sm">
+        © Copyright 2024 − <a href="/">Amlan Buragohain</a>. All rights reserved. This site is not finished and never will be.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
