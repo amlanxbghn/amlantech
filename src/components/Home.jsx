5095302a8ac89db16983c33d952052b6b9893fc2
amlanxbghn <amlanxconnect@gmail.com>
2024-08-09 22:41:33 +0530

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <section id="home" className='min-h-screen'>
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is the Home section.</p>
-    </section>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="ram w-[80vw] mx-auto">
+        <h1 className='text-[70px] leading-[84px] w-[75%]'>
+            Currently working on scaling my Agency&nbsp;
+            <a href="/" target="_blank" rel="noopener noreferrer" className='underline'>
+                <span className='text-[#767676]'>
+                    ComingSoon
+                </span>
+            </a>
+            , and growing my&nbsp;
+            <a href="/" target="_blank" rel="noopener noreferrer" className='underline'>
+                Saas&nbsp;
+                <span className='text-[#767676]'>
+                    ComingSoon.io
+                </span>
+            </a>
+            .
+        </h1>
+      </div>
+    </div>
   );
 };
 
